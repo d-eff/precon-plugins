35f5194824ea5f94bced6ec33c0d15ddfc16739e
David Fernandez <dfernandez06@gmail.com>
2015-01-25 22:51:40 -0500

    Change output to be a little easier to work with

--- precon-export.php
+++ precon-export.php
@@ -43,23 +43,42 @@ function precon_export_run() {
 	$filename = 'forecast_data_' . time() . '.csv';
 
 	$header_row = array(
-		0 => 'id',
-		1 => 'title',
-		2 => 'value',
-		3 => 'key',
+		0 => 'ID',
+		1 => 'Forecast Title',
+		2 => 'Key',
+		3 => 'Values',
 	);
 
 	$data_rows = array();
 
 	global $wpdb, $bp;
-	$results = $wpdb->get_results('select p.id, p.post_title, m.meta_value, m.meta_key from wp_fsko_posts p inner join wp_fsko_preconforecasts f on p.id=f.postid inner join wp_fsko_postmeta m on p.id=m.post_id where m.meta_key like \'historicalVotesAdmin\' OR m.meta_key like \'historicalVotesExpert\' OR m.meta_key like \'historicalVotesSub\'');
+	$results = $wpdb->get_results('select p.id, p.post_title, m.meta_key, m.meta_value from wp_fsko_posts p inner join wp_fsko_preconforecasts f on p.id=f.postid inner join wp_fsko_postmeta m on p.id=m.post_id where m.meta_key like \'historicalVotesAdmin\' OR m.meta_key like \'historicalVotesExpert\' OR m.meta_key like \'historicalVotesSub\'');
 
 	foreach ( $results as $res ) {
 		$row = array();
-		$row[0] = $res->id;
-		$row[1] = $res->post_title;
-		$row[2] = $res->meta_value;
-		$row[3] = $res->meta_key;
+		$row[] = $res->id;
+		$row[] = $res->post_title;
+		$mkey = $res->meta_key;
+
+		switch ($mkey) {
+			case 'historicalVotesAdmin':
+				$mkey = 'House';
+				break;
+			
+			case 'historicalVotesExpert':
+				$mkey = 'Expert';
+				break;
+
+			case 'historicalVotesSub':
+				$mkey = 'Subscriber';
+				break;
+		}
+
+		$row[] = $mkey;
+		$vals = unserialize($res->meta_value);
+		foreach ($vals as $key => $value) {
+			$row[] = $key . ' ' . $value;	
+		}
 
 		$data_rows[] = $row;
 	}
